a64e22d62e0fda1be7e0359e28f044b5af264555
Tyler DiPietro <tylerdipietro@Tylers-MacBook-Pro-3.local>
2020-04-16 12:46:55 -0400

    Logout Fixed

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -23,7 +23,7 @@ router.get('/', auth, async (req, res) => {
 });
 
 // @route   POST api/auth
-// @desc    Authenticat user and get token
+// @desc    Authenticate user and get token
 // @access  Public
 router.post(
   '/',
